73748ff9c292170d9a8a368235501c33c0b5e5ef
Emmanuel Santos <e.ss.rdz@gmail.com>
2021-02-25 18:39:58 -0600

    Nuevos campos para modelos
    Se añadió id para Historia y Usuario y proyecto para Historia

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,7 +1,8 @@
 // Usuario.js
 // Esta clase representa a los usuarios, ya sean desarrolladores o supervisores
 class Usuario{
-    constructor(nombre, cargo, departamento){
+    constructor(id, nombre, cargo, departamento){
+        this.id = id;
         this.nombre = nombre;
         this.cargo = cargo;
         this.departamento = departamento;
