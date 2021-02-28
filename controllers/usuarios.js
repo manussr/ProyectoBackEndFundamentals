ece53459874e93063ae78115d8d745a31cbb8b72
Edder Serna <edderserna22@gmail.com>
2021-02-27 21:51:27 -0600

    Correccion de errores

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,5 @@
 const Usuario = require('../models/Usuario');
-const Historia = require('../models/Usuario');
+
 
 // Crea un usuario
 function crearUsuario(req, res) {
@@ -24,7 +24,7 @@ function eliminarUsuario(req, res) {
 function obtenerUsuarios(req, res) {
     const usuario1 = new Usuario(1, 'Edder Serna', 'Supervisor', 'Control de Desarrollo');
     const usuario2 = new Usuario(2, 'Alan Garcia', 'Desarrollador', 'Desarrollo CSS');
-    res.status(201).send([historia1, historia2]);
+    res.status(201).send([usuario1, usuario2]);
 }
 
 
