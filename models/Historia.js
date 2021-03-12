8ad134754a7b7974d23e370c0ff5649043773ccb
Edder Serna <edderserna22@gmail.com>
2021-03-11 19:49:32 -0600

    Correcciones Menores

--- models/Historia.js
+++ models/Historia.js
@@ -1,6 +1,6 @@
 // Historia.js
 // Esta clase representa las historias (tareas) a desarrollar
-class Historia{
+/* class Historia{
     constructor(id, nombre, descripcion, fechaCreacion, fechaTermino, idCreador, idResponsable, proyecto, prioridad, estado){
         this.id = id;
         this.nombre = nombre;
@@ -15,4 +15,39 @@ class Historia{
     }
 }
 
-module.exports = Historia;
+module.exports = Historia; */
+
+// importamos por separado los métodos de Sequelize y los tipos de dato.
+const { Sequelize, DataTypes } = require('sequelize');
+// importamos sequelize con la opción de memory para forzar al gestor a almacenarla en la memoria.
+const sequelize = new Sequelize('mysql::memory:');
+
+//creanto el modelo para la entidad Historia
+const Proyecto = sequelize.define('Proyecto', {
+
+
+    id: {
+        // se indica el tipo de dato de la columna.
+        type: DataTypes.INTEGER,
+        // indicamos que este campo es llave primaria
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        // indicamos que el campo no admite valores null
+        allowNull: false
+    },
+    nombre: DataTypes.STRING,
+    apellido: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    ubicacion: DataTypes.STRING,
+    telefono: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    fotos: DataTypes.STRING,
+    tipo: DataTypes.INTEGER
+        // le decimos a que tabla de nuestra base de datos corresponde.
+}, { tableName: 'usuario' });
+
+// exportamos el modelo.
+module.exports = Usuario;
