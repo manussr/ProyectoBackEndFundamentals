1975b876a7a46e552ad5d419f636ee265e00693a
Emmanuel Santos <e.ss.rdz@gmail.com>
2021-03-14 20:57:21 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 module.exports = {
 
     database: {
-        username: "DVxKsCODLM",
-        password: "0t4pPr3pi8",
-        database: "DVxKsCODLM",
-        host: "remotemysql.com"
-    }
+        username: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        host: process.env.HOST
+    },
+    secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
 
 }
